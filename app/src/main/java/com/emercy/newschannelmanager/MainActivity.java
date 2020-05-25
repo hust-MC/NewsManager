5f44f5e52b9a316da11617d6bad5a80ae2b890c0
machao10 <machao10@baidu.com>
2020-05-25 15:49:44 +0800

    my manager

---
+++ app/src/main/java/com/emercy/newschannelmanager/MainActivity.java
@@ -0,0 +1,67 @@
+package com.emercy.newschannelmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private GridView mOtherGv;
+    private GridView mUserGv;
+    private List<String> mUserList = new ArrayList<>();
+    private List<String> mOtherList = new ArrayList<>();
+    private OtherAdapter mChannelAdapter;
+    private UserAdapter mUserAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+
+    public void initView() {
+        mUserGv = findViewById(R.id.userGridView);
+        mOtherGv = findViewById(R.id.otherGridView);
+        mUserList.add("推荐");
+        mUserList.add("热点");
+        mUserList.add("上海");
+        mUserList.add("时尚");
+        mUserList.add("科技");
+        mUserList.add("体育");
+        mUserList.add("军事");
+        mUserList.add("财经");
+        mUserList.add("网络");
+        mOtherList.add("汽车");
+        mOtherList.add("房产");
+        mOtherList.add("社会");
+        mOtherList.add("情感");
+        mOtherList.add("女人");
+        mOtherList.add("旅游");
+        mOtherList.add("健康");
+        mOtherList.add("美女");
+        mOtherList.add("游戏");
+        mOtherList.add("数码");
+        mOtherList.add("娱乐");
+        mOtherList.add("探索");
+        mUserAdapter = new UserAdapter(this, mUserList);
+        mChannelAdapter = new OtherAdapter(this, mOtherList);
+        mUserGv.setAdapter(mUserAdapter);
+        mOtherGv.setAdapter(mChannelAdapter);
+        mUserGv.setOnItemClickListener(this);
+        mOtherGv.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
---
+++ app/src/main/java/com/emercy/newschannelmanager/OtherAdapter.java
@@ -0,0 +1,58 @@
+package com.emercy.newschannelmanager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class OtherAdapter extends BaseAdapter {
+
+    private List<String> mList;
+    private Context mContext;
+    private ViewHolder mViewHolder;
+
+    OtherAdapter(Context context, List<String> otherList) {
+        mContext = context.getApplicationContext();
+        mList = otherList;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.adapter_mygridview_item, null);
+        }
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+        if (viewHolder == null) {
+            viewHolder = new ViewHolder();
+            viewHolder.tv = convertView.findViewById(R.id.text_item);
+            convertView.setTag(viewHolder);
+        }
+
+        viewHolder.tv.setText(mList.get(position));
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView tv;
+    }
+}
